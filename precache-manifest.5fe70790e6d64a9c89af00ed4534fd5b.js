8787deeddddb5076fbf7516194f2cc7694a9defc
Bijay Subedi <66840690+subedibj@users.noreply.github.com>
2020-08-16 13:44:32 -0700

    Updates

---
+++ precache-manifest.5fe70790e6d64a9c89af00ed4534fd5b.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8af0f071372795e01405d812af5dec15",
+    "url": "/reactproject1/index.html"
+  },
+  {
+    "revision": "3a21a2c225c70fdda996",
+    "url": "/reactproject1/static/css/main.98c559e2.chunk.css"
+  },
+  {
+    "revision": "1039d781fafec178dd52",
+    "url": "/reactproject1/static/js/2.3940f3fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/reactproject1/static/js/2.3940f3fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a21a2c225c70fdda996",
+    "url": "/reactproject1/static/js/main.9e4ee1fe.chunk.js"
+  },
+  {
+    "revision": "a33c4535c62d48f2392a",
+    "url": "/reactproject1/static/js/runtime-main.a39663e7.js"
+  },
+  {
+    "revision": "5cea79da66b347f17f160b448f33560d",
+    "url": "/reactproject1/static/media/e-commerce.5cea79da.jpg"
+  },
+  {
+    "revision": "4fbe4b587eafcbfae1c8f715c41265f1",
+    "url": "/reactproject1/static/media/img0.4fbe4b58.jpg"
+  },
+  {
+    "revision": "60f387e0c9cc8dd7cd006be80f7403cf",
+    "url": "/reactproject1/static/media/img1.60f387e0.jpg"
+  },
+  {
+    "revision": "8281b18c5f554f6b72269a6b2ea792ed",
+    "url": "/reactproject1/static/media/img2.8281b18c.jpg"
+  },
+  {
+    "revision": "3919818d13454d8f8d3a5628a6a2308e",
+    "url": "/reactproject1/static/media/img5.3919818d.jpg"
+  },
+  {
+    "revision": "50250572b70b831af6efd8bd2ea5ba71",
+    "url": "/reactproject1/static/media/men1.50250572.jpg"
+  },
+  {
+    "revision": "9a8e04247a8490b19f97799912dc6d58",
+    "url": "/reactproject1/static/media/men2.9a8e0424.jpg"
+  },
+  {
+    "revision": "acf21f32ff6ab1f45976cf412fff4475",
+    "url": "/reactproject1/static/media/men4.acf21f32.jpg"
+  },
+  {
+    "revision": "400c7815c062d897ca3e1f425caa504f",
+    "url": "/reactproject1/static/media/organization.400c7815.png"
+  },
+  {
+    "revision": "6b3824fd130d2c3ecc862866877f6141",
+    "url": "/reactproject1/static/media/schoolsoftware.6b3824fd.jpg"
+  },
+  {
+    "revision": "2d44032aff1af7ee81fdcd528705727a",
+    "url": "/reactproject1/static/media/woman2.2d44032a.jpg"
+  }
+]);
